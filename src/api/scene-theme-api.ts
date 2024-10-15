0911ae32a748edf0e79c63b064a1857b74cd2dc2
杨继康 <jack_87@163.com>
2024-10-15 11:20:28 +0800

    调整数据请求

---
+++ src/api/scene-theme-api.ts
@@ -0,0 +1,58 @@
+import { SceneThemeGrouproup, SceneThemeItem } from "@/model/theme-item";
+
+// 获取主题列表
+export async function getSceneThemeList() {
+  const studySceneThemeList: SceneThemeItem[] = [
+    {
+      id: "1",
+      title: "旅行",
+      description: "谈一谈你的旅行",
+      image:
+        "https://images.pexels.com/photos/386009/pexels-photo-386009.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
+      progress: 0.5,
+    },
+    {
+      id: "2",
+      title: "美食",
+      description: "谈一谈你的美食",
+      image:
+        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
+      progress: 0,
+    },
+    {
+      id: "3",
+      title: "美食",
+      description: "谈一谈你的美食",
+      image:
+        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
+      progress: 0,
+    },
+    {
+      id: "4",
+      title: "美食",
+      description: "谈一谈你的美食",
+      image:
+        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
+      progress: 0,
+    },
+  ];
+
+  const SceneThemeGroupList: SceneThemeGrouproup[] = [
+    {
+      id: "1",
+      title: "旅行",
+      items: studySceneThemeList,
+    },
+    {
+      id: "2",
+      title: "美食",
+      items: studySceneThemeList,
+    },
+    {
+      id: "3",
+      title: "生活",
+      items: studySceneThemeList,
+    },
+  ];
+  return SceneThemeGroupList;
+}
--- src/app/(default)/scene/page.tsx
+++ src/app/(default)/scene/page.tsx
@@ -1,63 +1,14 @@
-"use client";
 import MaxWindthWrapper from "@/components/MaxWidathWrapper";
-import { SceneThemeGrouproup, SceneThemeItem } from "@/model/theme-item";
+import { SceneThemeItem } from "@/model/theme-item";
 import Image from "next/image";
 import { Progress } from "@/components/ui/progress";
-export default function ScenePage() {
-  //当前学习的场景主题列表
-  const studySceneThemeList: SceneThemeItem[] = [
-    {
-      id: "1",
-      title: "旅行",
-      description: "谈一谈你的旅行",
-      image:
-        "https://images.pexels.com/photos/386009/pexels-photo-386009.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
-      progress: 0.5,
-    },
-    {
-      id: "2",
-      title: "美食",
-      description: "谈一谈你的美食",
-      image:
-        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
-      progress: 0,
-    },
-    {
-      id: "3",
-      title: "美食",
-      description: "谈一谈你的美食",
-      image:
-        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
-      progress: 0,
-    },
-    {
-      id: "4",
-      title: "美食",
-      description: "谈一谈你的美食",
-      image:
-        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=300&h=200&dpr=1",
-      progress: 0,
-    },
-  ];
-
+import { getSceneThemeList } from "@/api/scene-theme-api";
+export default async function ScenePage() {
   //其他各主题列表
-  const SceneThemeGroupList: SceneThemeGrouproup[] = [
-    {
-      id: "1",
-      title: "旅行",
-      items: studySceneThemeList,
-    },
-    {
-      id: "2",
-      title: "美食",
-      items: studySceneThemeList,
-    },
-    {
-      id: "3",
-      title: "生活",
-      items: studySceneThemeList,
-    },
-  ];
+  const SceneThemeGroupList = await getSceneThemeList();
+
+  //当前学习的场景主题列表
+  const studySceneThemeList = SceneThemeGroupList[0].items;
 
   return (
     <MaxWindthWrapper>
@@ -65,7 +16,7 @@ export default function ScenePage() {
         <div className="">
           {titleDiv("当前学习")}
           <div className="flex overflow-x-auto whitespace-nowrap">
-            {studySceneThemeList.map((item) => sceneThemeItemFunction(item))}
+            {studySceneThemeList.map((item) => sceneThemeItemFunction(item, true))}
           </div>
         </div>
         <div className="pt-10">
@@ -74,7 +25,7 @@ export default function ScenePage() {
             <div className="pt-5" key={group.id}>
               <div className="text-xl font-bold">{group.title}</div>
               <div className="flex max-w-screen overflow-x-auto whitespace-nowrap">
-                {group.items.map((item) => sceneThemeItemFunction(item,true))}
+                {group.items.map((item) => sceneThemeItemFunction(item))}
               </div>
             </div>
           ))}
@@ -84,7 +35,7 @@ export default function ScenePage() {
   );
 
   //显示主题
-  function sceneThemeItemFunction(item: SceneThemeItem, isNotStudy?: boolean) {
+  function sceneThemeItemFunction(item: SceneThemeItem, isStudy?: boolean) {
     return (
       <div
         className="flex shadow-md flex-none justify-between w-96 m-5 bg-themeItemBac rounded-lg items-center hover:bg-hoverThemeItemBac hover:text-white cursor-pointer"
@@ -93,9 +44,9 @@ export default function ScenePage() {
         <div className="p-5 flex flex-col gap-3 w-full">
           <div className="text-lg font-bold">{item.title}</div>
           <div>{item.description}</div>
-          {isNotStudy ?? (
+          {isStudy ? (
             <Progress className="w-full " value={item.progress * 100} />
-          )}
+          ): (<div></div>)}
         </div>
         <Image
           className="rounded-l-full"
